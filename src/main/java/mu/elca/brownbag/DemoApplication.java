fe2c207598855924597e4e050671fe5ae4fcd314
Shade <Shade@Shade.ga>
2024-03-17 17:37:41 +0400

    Initial commit

---
+++ src/main/java/mu/elca/brownbag/DemoApplication.java
@@ -0,0 +1,13 @@
+package mu.elca.brownbag;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
