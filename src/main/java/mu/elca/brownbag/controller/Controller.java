0cb42b223af7c08c175e176fb963e0659ce452d4
Shade <Shade@Shade.ga>
2024-03-18 20:48:42 +0400

    Initial Commit

--- src/main/java/mu/elca/brownbag/controller/Controller.java
+++ src/main/java/mu/elca/brownbag/controller/Controller.java
@@ -5,9 +5,11 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api")
 public class Controller {
 
     @GetMapping("/me")
