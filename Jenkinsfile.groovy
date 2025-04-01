c46658796257187ea058d10c924a140c232756ad
Shade <Shade@Shade.ga>
2025-04-01 23:56:14 +0400

    Jenkinsfile

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,5 +1,10 @@
 pipeline {
-    agent any
+    agent {
+        kubernetes {
+            label 'jnlp'
+            defaultContainer 'jnlp'
+        }
+    }
 
     tools {
         jdk 'openjdk-17'
