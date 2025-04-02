1b76cce1bb1037f8544069e3e63886d07103e697
Shade <Shade@Shade.ga>
2025-04-02 17:21:11 +0400

    Jenkinsfile

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -11,34 +11,11 @@ pipeline {
         dockerTool 'docker'
     }
 
-    environment {
-        DOCKER_REGISTRY = 'registry-server.devops-tools.svc.cluster.local:5000'
-    }
-
     stages {
-        stage('Ping Registry') {
-            steps {
-                script {
-                    def registryHost = 'registry-server.devops-tools.svc.cluster.local'
-                    def registryPort = '5000'
-                    sh "ping -c 3 ${registryHost} || echo 'Ping failed, moving to curl test'"
-                    sh "curl -v http://${registryHost}:${registryPort}/v2/_catalog || echo 'Registry is not reachable'"
-                }
-            }
-        }
 
         stage('Checkout') {
             steps {
                 checkout scm
-            }
-        }
-
-        stage('Extract Project Info') {
-            steps {
-                script {
-                    env.IMAGE_NAME = sh(script: "mvn help:evaluate -Dexpression=project.artifactId -q -DforceStdout", returnStdout: true).trim()
-                    env.IMAGE_VERSION = sh(script: "mvn help:evaluate -Dexpression=project.version -q -DforceStdout", returnStdout: true).trim()
-                }
             }
         }
 
@@ -56,17 +33,6 @@ pipeline {
             }
         }
 
-        stage('Push Docker Image') {
-            steps {
-                script {
-                    container('dind') {
-                        def fullImageName = "${DOCKER_REGISTRY}/${env.IMAGE_NAME}:${env.IMAGE_VERSION}"
-                        sh "docker tag ${env.IMAGE_NAME}:${env.IMAGE_VERSION} ${fullImageName}"
-                        sh "docker push ${fullImageName}"
-                    }
-                }
-            }
-        }
     }
 
     post {
