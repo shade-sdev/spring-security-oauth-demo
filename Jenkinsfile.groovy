1e0d15b8dd7a845afe20c51202f234107921b0d1
Shade <Shade@Shade.ga>
2025-04-02 17:54:00 +0400

    Jenkinsfile

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -11,28 +11,58 @@ pipeline {
         dockerTool 'docker'
     }
 
-    stages {
+    environment {
+        REGISTRY = "registry-server.devops-tools.svc.cluster.local:5000"
+    }
 
+    stages {
         stage('Checkout') {
             steps {
                 checkout scm
             }
         }
 
-        stage('Build') {
-            steps {
-                sh ''
-            }
-        }
-
-        stage('Build Docker Image using Jib') {
+        stage('Build and Push Docker Image using Jib') {
             steps {
                 script {
-                    sh 'mvn compile jib:build'
+                    def artifactId = sh(script: 'mvn help:evaluate -Dexpression=project.artifactId -q -DforceStdout', returnStdout: true).trim()
+                    def version = sh(script: 'mvn help:evaluate -Dexpression=project.version -q -DforceStdout', returnStdout: true).trim()
+                    def image = "${env.REGISTRY}/${artifactId}:${version}"
+
+                    echo "Building and pushing image: ${image}"
+                    sh "mvn compile jib:build -Djib.to.image=${image}"
+
+                    // Store image name for later use
+                    env.IMAGE_NAME = image
                 }
             }
         }
 
+        stage('Deploy Pod using Kubernetes Plugin') {
+            steps {
+                script {
+                    podTemplate(
+                            containers: [
+                                    containerTemplate(
+                                            name: 'app-container',
+                                            image: env.IMAGE_NAME,
+                                            alwaysPullImage: true,
+                                            command: 'cat', // Keeps the container running
+                                            ttyEnabled: true,
+                                            ports: [portMapping(8921, 8921)]
+                                    )
+                            ]
+                    ) {
+                        node(POD_LABEL) {
+                            container('app-container') {
+                                echo '✅ Application pod started successfully!'
+                                sh 'echo "Application running inside Kubernetes Pod"'
+                            }
+                        }
+                    }
+                }
+            }
+        }
     }
 
     post {
